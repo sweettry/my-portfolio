2721ea0ee4c2be2e520c9fa518b3a899a9723a79
sweettry <105031615+sweettry@users.noreply.github.com>
2024-08-23 12:18:03 -0500

    installed db, seeded db, added basic query, definitions

--- app/(dashboard)/projects/layout.tsx
+++ app/(dashboard)/projects/layout.tsx
@@ -1,16 +1,53 @@
 import ProjectsNavbar from '@/components/ProjectsNavbar';
 import { Card } from '@/components/ui/card';
-
-const layout = ({
+import { fetchProjects } from '@/lib/data';
+import clsx from 'clsx';
+import Image from 'next/image';
+const layout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const projects = await fetchProjects();
+
   return (
     <section className='container mx-auto h-[65vh] max-w-6xl px-4 text-center md:px-6'>
       <Card className='bg-custom-gradient grid w-full grid-cols-1 gap-8 text-white md:grid-cols-[1fr_4fr] md:gap-12 lg:gap-16'>
         <div className='h-96'>
           <h1>Projects</h1>
+          <div className='bg-white px-6'>
+            {projects.map((project, i) => {
+              return (
+                <div
+                  key={project.id}
+                  className={clsx(
+                    'flex flex-row items-center justify-between py-4',
+                    {
+                      'border-t': i !== 0,
+                    }
+                  )}
+                >
+                  <div className='flex items-center'>
+                    <Image
+                      src={project.image_url}
+                      alt={`${project.title}'s profile picture`}
+                      className='mr-4 rounded-full'
+                      width={32}
+                      height={32}
+                    />
+                    <div className='min-w-0'>
+                      <p className='truncate text-sm font-semibold md:text-base'>
+                        {project.title}
+                      </p>
+                      <p className='hidden text-sm text-gray-500 sm:block'>
+                        {project.description}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
           <ProjectsNavbar />
         </div>
         <div className='h-96'>{children}</div>
---
+++ lib/placeholder-data.ts
@@ -0,0 +1,23 @@
+const projects = [
+  {
+    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    title: 'Evil Rabbit',
+    description: 'evil@rabbit.com',
+    image_url: '/customers/evil-rabbit.png',
+  },
+  {
+    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+    title: 'Delba de Oliveira',
+    description: 'delba@oliveira.com',
+    image_url: '/customers/delba-de-oliveira.png',
+  },
+
+  {
+    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+    title: 'Balazs Orban',
+    description: 'balazs@orban.com',
+    image_url: '/customers/balazs-orban.png',
+  },
+];
+
+export { projects };
